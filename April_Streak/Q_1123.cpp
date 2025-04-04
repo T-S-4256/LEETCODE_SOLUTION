a07abaf156310707628e15a1dd9d3e2bb36a6dcd
T-S-4256 <sp4256824@gmail.com>
2025-04-04 17:22:50 +0530

    4th April Solution

---
+++ April_Streak/Q_1123.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    void depth(TreeNode *root, int d)
+    {
+        if (!root)
+        {
+            return;
+        }
+
+        maxD = max(maxD, d);
+        mp[root->val] = d;
+        depth(root->left, d + 1);
+        depth(root->right, d + 1);
+    }
+    // T.C : O(n)
+    // S.C : O(maxD) system recursion stack space
+    TreeNode *lcaDeepestLeaves(TreeNode *root)
+    {
+        depth(root, 0);
+
+        return LCA(root);
+    }
+    unordered_map<int, int> mp;
+    int maxD = 0;
+
+    TreeNode *LCA(TreeNode *root)
+    {
+        if (root == NULL || mp[root->val] == maxD)
+        {
+            return root;
+        }
+
+        TreeNode *l = LCA(root->left);
+        TreeNode *r = LCA(root->right);
+
+        if (l && r)
+        {
+            return root;
+        }
+
+        return l != NULL ? l : r;
+    }
+};
+
+
+int main(){
+
+
+    return 0;
+}
