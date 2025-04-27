30b08a7218a8fd161a60f0e402aaebbfd3db5ec5
T-S-4256 <sp4256824@gmail.com>
2025-04-27 15:21:23 +0530

    27th April solution

---
+++ April_Streak/3392.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int countSubarrays(vector<int> &nums)
+    {
+        int count = 0;
+        for (int i = 1; i < nums.size() - 1; i++)
+        {
+            cout << nums[i] << " : " << (2 * (nums[i - 1] + nums[i + 1])) << endl;
+            if (nums[i] == (2 * (nums[i - 1] + nums[i + 1])))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    // CASE 1:
+    // Input: nums = [1,1,1]
+    // Output: 0
+    // CASE 2:
+    // Input: nums = [1,2,1,4,1]
+    // Output: 1
+
+    Solution s;
+    vector<int> nums = {1, 2, 1, 4, 1};
+    cout << "Ans  : " << s.countSubarrays(nums);
+
+    return 0;
+}
